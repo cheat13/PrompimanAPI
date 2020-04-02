ea5cbc7dcb30db226ab00289fcf7ad97284af99f
Kritsana <tragoolphrom@gmail.com>
2020-04-02 14:50:11 +0700

    edit Model

--- Models/EnMember.cs
+++ Models/EnMember.cs
@@ -5,10 +5,10 @@ namespace PrompimanAPI.Models
     public class EnMember
     {
         public string PassportNo { get; set; }
-        public string En_Prefix { get; }
-        public string En_Firstname { get; }
-        public string En_Lastname { get; }
-        public string Sex { get; }
+        public string En_Prefix { get; set; }
+        public string En_Firstname { get; set; }
+        public string En_Lastname { get; set; }
+        public string Sex { get; set; }
         public DateTime Birthday { get; set; }
         public DateTime IssueDate { get; set; }
         public DateTime ExpiryDate { get; set; }
--- Models/ThMember.cs
+++ Models/ThMember.cs
@@ -5,13 +5,13 @@ namespace PrompimanAPI.Models
     public class ThMember
     {
         public string IDCard { get; set; }
-        public string Th_Prefix { get; }
-        public string Th_Firstname { get; }
-        public string Th_Lastname { get; }
-        public string En_Prefix { get; }
-        public string En_Firstname { get; }
-        public string En_Lastname { get; }
-        public string Sex { get; }
+        public string Th_Prefix { get; set; }
+        public string Th_Firstname { get; set; }
+        public string Th_Lastname { get; set; }
+        public string En_Prefix { get; set; }
+        public string En_Firstname { get; set; }
+        public string En_Lastname { get; set; }
+        public string Sex { get; set; }
         public DateTime Birthday { get; set; }
         public string Address { get; set; }
         public DateTime IssueDate { get; set; }
