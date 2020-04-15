d1095a645ab7329df3132f9e97263785934c6514
Kritsana <tragoolphrom@gmail.com>
2020-04-15 16:12:20 +0700

    implement RoomController

---
+++ Models/DateRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PrompimanAPI.Models
+{
+    public class DateRequest
+    {
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+    }
+}
